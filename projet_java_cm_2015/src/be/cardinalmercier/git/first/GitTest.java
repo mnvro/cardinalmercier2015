9ef5c0c8a5e1eab7f4411b67392cc433d9afcd1e
mnvro <mnvro@hotmail.com>
2015-02-26 13:20:42 +0100

    Tout premier Commit

---
+++ projet_java_cm_2015/src/be/cardinalmercier/git/first/GitTest.java
@@ -0,0 +1,9 @@
+package be.cardinalmercier.git.first;
+
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("Git is Fun!");
+		System.out.println("Voici un changement");
+	}
+
+}
