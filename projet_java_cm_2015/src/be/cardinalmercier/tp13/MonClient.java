fc474a88e81c67db2b8e65c5e62555dab2a3ebbc
mnvro <mnvro@hotmail.com>
2015-04-30 20:23:50 +0200

    ok

---
+++ projet_java_cm_2015/src/be/cardinalmercier/tp13/MonClient.java
@@ -0,0 +1,7 @@
+package be.cardinalmercier.tp13;
+
+import java.net.Socket;
+
+public class MonClient extends Socket {
+
+}
--- projet_java_cm_2015/src/be/cardinalmercier/tp13/MonServeurTCP.java
+++ projet_java_cm_2015/src/be/cardinalmercier/tp13/MonServeurTCP.java
@@ -2,26 +2,47 @@ package be.cardinalmercier.tp13;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
-public class MonServeurTCP extends ServerSocket {
+public class MonServeurTCP extends ServerSocket implements Runnable {
 	private int numPort ;
+	private Socket client;
 	public MonServeurTCP() throws IOException {
 		super(Param.numPort);
 		numPort = Param.numPort;
+		Thread t = new Thread(this);
+		t.start();
+		
+		
 	}
 	public static void main(String[] args) {
 		MonServeurTCP ms = null;
-		for (int i = Param.numPort ; i < 65535 ; i++){
+		int i;
+		for (i = Param.numPort ; i < 65536 ; i++){
 			try {
 				ms = new MonServeurTCP();
-				System.out.println("le serveur écoute sur le port : "+Param.numPort);
+				System.out.println("le serveur écoute sur le port : "+ms.numPort);
 				break;
 			} catch (IOException e) {
 				e.printStackTrace();
 				Param.numPort ++;
 			}
+		}
+		if (i == 65536){
 			System.out.println("Aucun port disponible");
 		}
+	}
+	@Override
+	public void run() {
+		try {
+			System.out.println("**** On attend un client....");
+			client = this.accept();
+			System.out.println("**** un nouveau client s'est connecté....");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
 
